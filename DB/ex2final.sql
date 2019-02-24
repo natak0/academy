427b15fe8fecac0c7290b06f1627a9bec4f55b96
Natako <bcnatako@gmail.com>
2019-02-24 19:22:50 +0100

    updated repository with API and sql exercise

--- DB/ex2final.sql
+++ DB/ex2final.sql
@@ -6,8 +6,8 @@ GROUP BY Origin;
 SELECT Origin, Year, Month, AVG(DepDelay) as Avg_delay FROM USAirlineFlights2.flights 
 Group by Month, Year, Origin Order by Origin, Year, Month;
 
-SELECT b.City, a.Year, a.Month, AVG(a.DepDelay) as Avg_delay FROM USAirlineFlights2.flights a, USAirlineFlights2.USAirports b 
-WHERE a.ORIGIN=b.IATA
+SELECT b.City, a.Year, a.Month, AVG(a.DepDelay) as Avg_delay FROM USAirlineFlights2.flights a LEFT JOIN USAirlineFlights2.USAirports b 
+ON a.ORIGIN=b.IATA
 Group by a.Month, a.Year, b.City Order by b.City, a.Year, a.Month;
 
 SELECT b.Description as CompaniesCancelledFlights, Count(a.Cancelled) FROM USAirlineFlights2.flights a, USAirlineFlights2.Carriers b 
@@ -22,8 +22,3 @@ SELECT b.Description, AVG(a.ArrDelay) as AvgArrivalDelay FROM USAirlineFlights2.
 WHERE a.UniqueCarrier=b.CarrierCode 
 Group by b.Description
 HAVING AvgArrivalDelay>'10';
-
-
-
-
-
