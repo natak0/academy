4b476c5b4f4fc5905970757e1de8574e1278cf5c
Natako <bcnatako@gmail.com>
2019-03-14 10:01:32 +0100

    added exercise in JS, create an array of full name and count how many repeated letters are in the name.

---
+++ JS exercises/js name arrays/scripts/script.js
@@ -0,0 +1,49 @@
+//1 create an array and fill it with name letters
+//var myname = ['N','a','t','a','l','i','a'];
+var nombre = "Natalia1";
+myname=nombre.split('');
+console.log(myname);
+
+//show the length of array
+console.log(myname.length);
+//2 display in the console the item and its position in array
+myname.forEach(function(item, index, array) {
+  console.log(item, index);
+});
+
+//check for vocals and numbers
+for (var i=0; i<myname.length; i++){
+			if (myname[i] === 'a'|| myname[i] === 'i'|| myname[i] === 'e'|| myname[i] === 'u'|| myname[i] === 'o')console.log(myname[i], i, "vocal");
+			//check if there are any numbers
+			else if (!isNaN(myname[i])) {
+					alert("Name cannot contain numbers!");
+					console.log(myname[i],i, "number");
+				}
+			else console.log(myname[i], i, "consonant");	
+}
+//3 create a map
+nameMap = new Map();
+for(var i=0;i<myname.length;i++){
+	//return value element from myname array
+  	let number=nameMap.get(myname[i]);
+  	//console.log(number, "number");
+  	//if the letter already exists then increase value element
+	if (number) {
+   		nameMap.set(myname[i],number+1)
+	}
+	else {
+		nameMap.set(myname[i],1)
+	}
+}
+console.log(nameMap);
+
+
+//4 create a full name array with a space separator
+var surname = ['K','o','r','o','l','e','v','a'];
+var fullname = [];
+var sep = [' '];
+fullname = Array.from(myname);
+fullname.push.apply(fullname, sep);
+fullname.push.apply(fullname, surname);
+//fullname.push(surname);
+console.log(fullname);
